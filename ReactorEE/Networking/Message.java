0d443b337f6bfe2c5191a6949e44301ad39739aa
cjd515 <cjd515@york.ac.uk>
2013-03-20 16:27:56 +0000

    Update Message.java

--- ReactorEE/Networking/Message.java
+++ ReactorEE/Networking/Message.java
@@ -18,8 +18,8 @@ public class Message
 		socket.close();												
 	}
 	/**
-	 * Sends a string message to the described port.
-	 * @param message The byte array to be passed expressing the plant/hardware failure to be incurred.
+	 * Sends a byte array message to the described port. Intended for serialised Classes.
+	 * @param message The byte array to be passed expressing the serialised plant to be reproduced.
 	 * @param listenerIP The IP address of the other player's computer.
 	 * @param portNo The port number to connect to.
 	 */
