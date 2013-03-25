de96f08752ab3dc4adec2410a3ad941e7f61a952
Andrei Simionescu <ansimionescu@gmail.com>
2013-03-25 23:46:19 +0000

    sound.Music.java fully functional
    Must implement it in Swing now

--- ReactorEE/sound/Music.java
+++ ReactorEE/sound/Music.java
@@ -12,32 +12,48 @@ import javazoom.jl.player.Player;
 public class Music {
 	public static void main(String[] args) { // this is bleeding edge i.e. not finished code! to have fun with it, run and type in a,s,d as seen below (you might have to add the .jar in Libraries/ to your build path)
 		Music.play();
-		System.out.println("hi 14");
+		
 		char x = 'y'; Scanner sc = new Scanner(System.in);
 		while(x != 'q') {
 			x = sc.next().toCharArray()[0];
 			if(x == 'a') Music.play();
 			if(x == 's') Music.pause();
 			if(x == 'd') Music.kill();
+			if(x == 'f') Music.changeGameContext("game");
+			if(x == 'g') Music.changeGameContext("menu");
 		}
 	}
 	
 	// API
+	/**
+	 * Enter <pre>Music.play();</pre> to play/resume the current track. Make sure to change game context with <pre>Music.changeGameContext(String)</pre>
+	 * The default value for game context is "menu"
+	 */
 	public static void play() {
 		if(instance == null) setup();
 		instance.ps = PlaybackStatus.PLAY;
 	}
 	
+	/**
+	 * Enter <pre>Music.pause();</pre> to
+	 */
 	public static void pause() {
 		if(instance == null) setup();
 		instance.ps = PlaybackStatus.PAUSE;
 	}
 	
+	/**
+	 * Enter <pre>Music.kill();</pre> to clear memory and stop the music. Any command will reinitialize the class.
+	 */
 	public static void kill() {
 		if(instance == null) setup();
 		instance.ps = PlaybackStatus.CLOSE;
 	}
 	
+	/**
+	 * Valid: "menu" "game" (maybe "credits" in the future)
+	 * @param newGameContext
+	 */
 	public static void changeGameContext(String newGameContext) {
 		GameStatus new_gs = GameStatus.fromString(newGameContext);
 		
@@ -58,37 +74,70 @@ public class Music {
 			playlist.put(GameStatus.PLAYING, playing);
 			//playlist.put(GameStatus.CREDITS, credits);
 		}
-		System.out.println("hi 53");
+		
 		playback = new Thread(new Runnable() { public void run(){ instance.playback(); } });
 		playback.start();
 	}
 	
 	protected void playback() {
-		assert(instance != null);
-		Player player = createPlayer(gs,0); 
+		Player player = createPlayer(gs, 0);
+		boolean trackEnded = false;
+		GameStatus oldGS = gs;
+		int index = 0;
+		boolean _end = true;
+
+		while(_end) {
+			switch(ps) {
+			case CLOSE:
+				instance = null;
+				_end = false;
+				break;
+			case PLAY:
+				if(gs != oldGS) {
+					oldGS = gs;
+					trackEnded = false;
+					index = 0; 			System.out.println("hi 1");
+
+					player.close();
+					player = createPlayer(gs, index);
+				}
+				else if(trackEnded) {
+					trackEnded = false;
+					index = (index+1) % playlist.get(gs).size();
+					
+					player.close();			System.out.println("hi 2");
+					player = createPlayer(gs, index);
+				}
+				try { trackEnded = !player.play(8); } catch (Exception e) { System.out.println(e); } System.out.println("hi 3");
+				break;
+			case PAUSE:
+				try { Thread.sleep(500); } catch (InterruptedException e) { System.out.println(e); } System.out.println("hi 4");
+				break;
+			}
+		}
 		
-		while(true) {System.out.println("hi 61");
+		while(true) {
 			if(ps == PlaybackStatus.CLOSE) {
 				instance = null;
 				return;
-			} else if(ps == PlaybackStatus.PLAY) {
-				try { player.play(8); } catch (Exception e) { System.out.println(e); }
+			} else if(ps == PlaybackStatus.PLAY) { 
+				if(gs != oldGS) {
+					player = createPlayer(gs,0);
+					oldGS = gs;
+					trackEnded = false;
+					index = 0;
+				} else if(trackEnded) {
+					int size = playlist.get(gs).size();
+					player = createPlayer(gs, (index+1) % size);
+					trackEnded = false;
+				}
+				else {
+					try { trackEnded = player.play(8); } catch (Exception e) { System.out.println(e); }
+				}
 			} else {
-				try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+				try { Thread.sleep(500); } catch (InterruptedException e) { System.out.println(e); }
 			}
-		}
-	}
-
-	protected static void play(final GameStatus gs, final int index) {
-		if(instance == null) setup();
-		Thread playback = new Thread(new Runnable() {
-			public void run() {
-				try { createPlayer(gs,index).play(); }
-                catch (Exception e) { System.out.println(e); }
-			}
-		});
-		
-		playback.start();
+		} //end while(true)
 	}
 	
 	protected static Player createPlayer(GameStatus gs, int index) {
@@ -98,8 +147,7 @@ public class Music {
 		String filename = path + playlist.get(gs).get(index) + extension;
 
 		try {
-			out = new Player(new BufferedInputStream(new FileInputStream(
-					filename)));
+			out = new Player(new BufferedInputStream(new FileInputStream(filename)));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -145,7 +193,7 @@ enum GameStatus {
 	public static GameStatus fromString(String in) {
 		if(in.toLowerCase().equals("menu")) return MENU;
 		if(in.toLowerCase().equals("game")) return PLAYING;
-		if(in.toLowerCase().equals("menu")) return CREDITS;
+		if(in.toLowerCase().equals("credits")) return CREDITS;
 		
 		return null;
 	}
