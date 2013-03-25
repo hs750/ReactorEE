bbaec7df9dddeba57c22de542f8d5830895eca62
Andrei Simionescu <ansimionescu@gmail.com>
2013-03-25 22:55:37 +0000

    Rebuild Music to make it less stupid. Now play/pause work.
    Not possible to change tracks yet though

--- ReactorEE/sound/Music.java
+++ ReactorEE/sound/Music.java
@@ -2,150 +2,151 @@ package ReactorEE.sound;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.jl.player.Player;
 
 public class Music {
-	protected static final List<String> menu = Arrays.asList("menu-1", "menu-2");
-	protected static final List<String> playing = Arrays.asList("game-1", "game-2", "game-3", "game-4");
-	protected static final String path = "Music/";
-	protected static final String extension = ".mp3";
-	
-	protected Map<GameStatus, List<String>> files;
-	protected GameStatus status;
-	protected String currentlyPlaying;
-	protected AdvancedPlayer player;
-	protected Thread musicThread;
-	
-	// singleton stuff
-	private static Music instance = null;
-	private static void setup() { instance = new Music(); }
-	
-	private Music() {
-		status = GameStatus.MENU;
-		files =	new HashMap<GameStatus, List<String>>();
-		
-		files.put(GameStatus.MENU, menu);
-		files.put(GameStatus.PLAYING, playing);
-		
-		currentlyPlaying = path + files.get(GameStatus.MENU).get(0) + extension;
-		player = getPlayer(currentlyPlaying);
-	}
-	
-	// useful
-	protected AdvancedPlayer getPlayer(String filename) {
-		AdvancedPlayer out = null;
-		
-		try {
-			player = new AdvancedPlayer(new BufferedInputStream(new FileInputStream(filename)));
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (JavaLayerException e) {
-			System.out.println(e);
+	public static void main(String[] args) { // this is bleeding edge i.e. not finished code! to have fun with it, run and type in a,s,d as seen below (you might have to add the .jar in Libraries/ to your build path)
+		Music.play();
+		System.out.println("hi 14");
+		char x = 'y'; Scanner sc = new Scanner(System.in);
+		while(x != 'q') {
+			x = sc.next().toCharArray()[0];
+			if(x == 'a') Music.play();
+			if(x == 's') Music.pause();
+			if(x == 'd') Music.kill();
 		}
-		
-		return out;
 	}
 	
 	// API
 	public static void play() {
 		if(instance == null) setup();
-		
+		instance.ps = PlaybackStatus.PLAY;
 	}
 	
 	public static void pause() {
 		if(instance == null) setup();
-		
+		instance.ps = PlaybackStatus.PAUSE;
 	}
 	
-	public static void changeStatus(GameStatus newStatus) {
-		if(newStatus == instance.status)
-			return;
-		
-		instance.status = newStatus;
-		
-		// TODO add code to handle track change
-	}
-	
-	public static void play(GameStatus newStatus) {
+	public static void kill() {
 		if(instance == null) setup();
+		instance.ps = PlaybackStatus.CLOSE;
+	}
+	
+	public static void changeGameContext(String newGameContext) {
+		GameStatus new_gs = GameStatus.fromString(newGameContext);
 		
-		pause();
-		changeStatus(newStatus);
-		play();
+		if(new_gs == null)
+			System.err.println("Music.changeGameContext: Incorrect string for game context (possible values are \"menu\", \"game\", \"credits\")");
+		else
+			instance.gs = new_gs;
 	}
 	
-	public static Thread getThread() {
-		if(instance == null) setup();
-		return instance.musicThread;
+	// implementation
+	private Music() {
+		ps = PlaybackStatus.PAUSE;
+		gs = GameStatus.MENU;
+		
+		if(playlist == null) {
+			playlist = new HashMap<GameStatus, List<String>>();
+			playlist.put(GameStatus.MENU, menu);
+			playlist.put(GameStatus.PLAYING, playing);
+			//playlist.put(GameStatus.CREDITS, credits);
+		}
+		System.out.println("hi 53");
+		playback = new Thread(new Runnable() { public void run(){ instance.playback(); } });
+		playback.start();
 	}
 	
-	// legacy
-	;
-	public static void test() {
-		String filename = "Music/game-1.mp3";
-		MP3 mp3 = new MP3(filename);
-		mp3.play();
-
-		// do whatever computation you like, while music plays
-		int N = 15000;
-		double sum = 0.0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				sum += Math.sin(i + j);
+	protected void playback() {
+		assert(instance != null);
+		Player player = createPlayer(gs,0); 
+		
+		while(true) {System.out.println("hi 61");
+			if(ps == PlaybackStatus.CLOSE) {
+				instance = null;
+				return;
+			} else if(ps == PlaybackStatus.PLAY) {
+				try { player.play(8); } catch (Exception e) { System.out.println(e); }
+			} else {
+				try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
 			}
 		}
-		System.out.println(sum);
-
-		// when the computation is done, stop playing it
-		mp3.close();
-	}
-	public static void main(String[] args) { test(); }
-}
-
-
-
-class MP3 {
-	private String filename;
-	private AdvancedPlayer player;
-
-	// constructor that takes the path of an MP3 file
-	public MP3(String filename) {
-		this.filename = filename;
 	}
 
-	public void close() {
-		if (player != null)
-			player.close();
+	protected static void play(final GameStatus gs, final int index) {
+		if(instance == null) setup();
+		Thread playback = new Thread(new Runnable() {
+			public void run() {
+				try { createPlayer(gs,index).play(); }
+                catch (Exception e) { System.out.println(e); }
+			}
+		});
+		
+		playback.start();
 	}
+	
+	protected static Player createPlayer(GameStatus gs, int index) {
+		Player out = null;
+		
+		assert(playlist != null);
+		String filename = path + playlist.get(gs).get(index) + extension;
 
-	// play the MP3 file to the sound card
-	public void play() {
 		try {
-			FileInputStream fis = new FileInputStream(filename);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			player = new AdvancedPlayer(bis);
+			out = new Player(new BufferedInputStream(new FileInputStream(
+					filename)));
 		} catch (Exception e) {
-			System.out.println("Problem playing file " + filename);
 			System.out.println(e);
 		}
 
-		// run in new thread to play in background
-		new Thread() {
-			public void run() {
-				try {
-					player.play();
-				} catch (Exception e) {
-					System.out.println(e);
-				}
-			}
-		}.start();
+		return out;
 	}
+	
+	// fields
+	protected PlaybackStatus ps;
+	protected GameStatus gs;
+	protected Thread playback;
+	
+	// data
+	private static final List<String> menu = Arrays.asList("menu-1", "menu-2");
+	private static final List<String> playing = Arrays.asList("game-1", "game-2", "game-3", "game-4");
+	private static final String path = "Music/";
+	private static final String extension = ".mp3";
+	private static HashMap<GameStatus, List<String>> playlist = null;
+	
+	// singleton stuff
+	private static Music instance;
+	private static void setup() { instance = new Music(); }
 }
 
+
+enum PlaybackStatus {
+	PLAY, PAUSE, CLOSE
+}
+
+enum GameStatus {
+	MENU("menu"), PLAYING("game"), CREDITS("credits");
+	
+	private String filename;
+	private GameStatus(String s) {
+		filename = s;
+	}
+	
+	@Override
+	public String toString() {
+		return filename;
+	}
+	
+	public static GameStatus fromString(String in) {
+		if(in.toLowerCase().equals("menu")) return MENU;
+		if(in.toLowerCase().equals("game")) return PLAYING;
+		if(in.toLowerCase().equals("menu")) return CREDITS;
+		
+		return null;
+	}
+}
