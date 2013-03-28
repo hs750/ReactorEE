ed27073418bddc7fbf312b3c3ce61302820864df
Harrison <hs750@york.ac.uk>
2013-03-28 14:41:38 +0000

    test validation of IPs

--- ReactorEE/test/SocketUtilTest.java
+++ ReactorEE/test/SocketUtilTest.java
@@ -14,6 +14,10 @@ import ReactorEE.simulator.ReactorUtils;
 
 public class SocketUtilTest {
 
+	/**
+	 * Tests whether an instance of a plant is serialized and then unserialized correctly, resulting in the initial and unserialized version of the plant are the same.
+	 * 
+	 */
 	@Test
 	public void testSerialisationAndDeserialisationOfPlant() throws StreamCorruptedException, ClassNotFoundException, IOException {
 		ReactorUtils r = new ReactorUtils();
@@ -26,5 +30,31 @@ public class SocketUtilTest {
 		
 		assertEquals(p, p2);
 	}
+	
+	/**
+	 * Tests the validation of ip addresses. Valid and non valid ip addresses are tested.
+	 */
+	@Test
+	public void testValidateIP(){
+		boolean IPValid = false;
+		IPValid = SocketUtil.validateIP("123.123.123.123");
+		assertTrue(IPValid);
+		
+		IPValid = SocketUtil.validateIP("0.0.0.0");
+		assertTrue(IPValid);
+		
+		IPValid = SocketUtil.validateIP("255.255.255.255");
+		assertTrue(IPValid);
+		
+		IPValid = SocketUtil.validateIP("256.255.255.255");
+		assertTrue(!IPValid);
+		IPValid = SocketUtil.validateIP("255.256.255.255");
+		assertTrue(!IPValid);
+		IPValid = SocketUtil.validateIP("255.255.256.255");
+		assertTrue(!IPValid);
+		IPValid = SocketUtil.validateIP("255.255.255.256");
+		assertTrue(!IPValid);
+	}
+	
 
 }
