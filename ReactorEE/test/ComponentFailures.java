ce00b4cbf205227ad7a017b6a39d3335cabda08b
Harrison <hs750@york.ac.uk>
2013-03-28 12:42:10 +0000

    test reducing failure rate of components

--- ReactorEE/test/ComponentFailures.java
+++ ReactorEE/test/ComponentFailures.java
@@ -61,4 +61,85 @@ public class ComponentFailures {
 	   presenter.step(1);
 	   assertEquals(0, turbine.getRpm());
 	}
+	
+	//-------------- A4 Tests -----------------------
+	
+	/**
+	 * Tests whether reducing the failure rate of a pump results in it taking more steps for the pump to fail than a standard pump.
+	 */
+	@Test
+	public void testPumpReducedFaulureRate(){
+		Pump p1 = presenter.getPlant().getPumps().get(0);
+		Pump p2 = presenter.getPlant().getPumps().get(1);
+		
+		p2.setFailureRate(1);
+		p2.setMaxFailureRate(1);
+		
+		int p1FailTime = 0;
+		int p2FailTime = 0;
+		
+		while(p2.isOperational()){
+			if(p1.isOperational())
+				p1FailTime++;
+			p2FailTime++;
+			presenter.step(1);
+		}
+		
+		assertTrue(""+p1FailTime + " /< " + p2FailTime, p1FailTime < p2FailTime);
+		
+	}
+	
+	/**
+	 * Tests whether reducing the failure rate of a turbine increases the number of steps needed to result in a turbine failing.
+	 */
+	@Test
+	public void testTurbineReducedFaulureRate(){
+		Turbine t1 = turbine;
+		PlantController pc2 = new PlantController(new ReactorUtils());
+		Turbine t2 = pc2.getPlant().getTurbine();
+		
+		t2.setFailureRate(1);
+		t2.setMaxFailureRate(1);
+		
+		int t1FailTime = 0;
+		int t2FailTime = 0;
+		
+		while(t2.isOperational()){
+			if(t1.isOperational())
+				t1FailTime++;
+			t2FailTime++;
+			presenter.step(1);
+			pc2.step(1);
+		}
+		
+		assertTrue(""+t1FailTime + " /< " + t2FailTime, t1FailTime < t2FailTime);
+		
+	}
+	
+	/**
+	 * Tests whether reducing the failure rate of an Operator Software increases the number of steps required for the Operator Software to fail.
+	 */
+	@Test
+	public void testOperatorSoftwareReducedFaulureRate(){
+		OperatingSoftware o1 = presenter.getPlant().getOperatingSoftware();
+		PlantController pc2 = new PlantController(new ReactorUtils());
+		OperatingSoftware o2 = pc2.getPlant().getOperatingSoftware();
+		
+		o2.setFailureRate(1);
+		o2.setMaxFailureRate(1);
+		
+		int o1FailTime = 0;
+		int o2FailTime = 0;
+		
+		while(o2.isOperational()){
+			if(o1.isOperational())
+				o1FailTime++;
+			o2FailTime++;
+			presenter.step(1);
+			pc2.step(1);
+		}
+		
+		assertTrue(""+o1FailTime + " /< " + o2FailTime, o1FailTime < o2FailTime);
+		
+	}
 }
