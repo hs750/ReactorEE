07675315630ab3d7915f2861702f2f16f5d96c62
Harrison <hs750@york.ac.uk>
2013-05-01 10:07:54 +0100

    made handshake test more complete

--- ReactorEE/test/HandshakeTest.java
+++ ReactorEE/test/HandshakeTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import ReactorEE.Networking.HandshakeListener;
 import ReactorEE.Networking.HandshakeRequest;
+import ReactorEE.Networking.SocketUtil;
 import ReactorEE.swing.MultiplayerSelectionGUI;
 
 public class HandshakeTest {
@@ -22,16 +23,28 @@ public class HandshakeTest {
 	 */
 	@Test
 	public void testHandshake() throws UnknownHostException, IOException, InterruptedException {
+		int count = 0;
+		while(SocketUtil.portTaken(SocketUtil.HANDSHAKE_PORT_NO)){
+			count++;
+			if(count > Integer.MAX_VALUE)
+				fail();
+		}
+		
 		HandshakeListener hsl = new HandshakeListener(new MultiplayerSelectionGUI());
 		HandshakeRequest hsr = new HandshakeRequest(new MultiplayerSelectionGUI(), InetAddress.getLocalHost().getHostAddress());
 		
 		hsl.start();
-		Thread.sleep(500);
 		hsr.start();
-		Thread.sleep(500);
+		
+		count = 0;
+		while(hsl.isAlive()|| hsr.isAlive()){
+			count++;
+			if(count > Integer.MAX_VALUE)
+				fail();
+		}
 		
 		//After some time the listener is nolonger active.
-		assertTrue(!hsl.isAlive());
+		assertTrue("" + !hsl.isAlive() + " " + !hsr.isAlive(),!hsl.isAlive() && !hsr.isAlive());
 	}
 
 }
